c85203efa081b0978cd581159d1ddac37fd8eda5
NandoNunez29 <129799884+NandoNunez29@users.noreply.github.com>
2023-05-18 12:02:06 -0500

    Add files via upload

---
+++ C# Ejercicios/BalotoAutomatico.cs
@@ -0,0 +1,39 @@
+﻿Random jugarBaloto = new Random();
+int[] balotas = new int[5];
+bool op = false;
+int superBalota = jugarBaloto.Next(1,17);
+
+do
+{
+    for (int i = 0; i < 5; i++)
+    {
+        balotas[i] = jugarBaloto.Next(1,44);        
+    }
+    for (int i = 0; i < 4; i++)
+    {    
+        for (int x = i+1; x < 4; x++)
+        {
+            if((balotas[i]== balotas[x]))
+        {
+            op = true;            
+        }
+
+        }
+                
+    }
+} while(op == true);
+
+Array.Sort(balotas);
+    
+Console.Write("JUEGE SU BALOTO ASI PARA VER SI GANA ALGO Y ME CUENTA: \n\n");
+Console.WriteLine("B A L O T A S\t\t\t\t\tSUPER BALOTA");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"{balotas[i]}\t");
+
+}
+
+Console.WriteLine($"\t\t{superBalota}\n");
+Console.Write("Si te lo ganas pasa la Liga!");
+Console.Read();
